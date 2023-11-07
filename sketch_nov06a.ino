1acb924b79530c208363bd8118512bad31caf871
Sanjay Prasanth A S <84510522+sanjay3081@users.noreply.github.com>
2023-11-07 14:52:13 +0530

    Update sketch_nov06a.ino

--- sketch_nov06a.ino
+++ sketch_nov06a.ino
@@ -1,4 +1,4 @@
-#include <NoDelay.h> // nidelay library to make a delay
+#include <NoDelay.h> // nodelay library to create delays
 
 const int l_Pin = A0;    // Analog pin for LM35 temperature sensor   
 
@@ -7,14 +7,15 @@ const int tempThreshold = 30;  // Temperature threshold in degrees Celsius
 noDelay ledBlinkDelay1(250); // to make a delay of 250ms
 noDelay ledBlinkDelay2(500); // to make a delay of 500ms
 
+// setup function is used to initailize input and output ports
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);  // built in lead pin(13) to set as output
+  Serial.begin(9600);  // baud rate 9600 to make communication
 }
 
 void loop() {
-  int rawValue = analogRead(l_Pin);  // Read LM35 sensor value
-  float voltage = (rawValue / 1024.0) * 5.0;  // adc value converted to analog value
+  int iValue = analogRead(l_Pin);  // Read LM35 sensor value
+  float voltage = (iValue / 1024.0) * 5.0;  // adc value converted to analog value
   float temperatureC = (voltage - 0.5) * 100.0;  // Convert to temperature in °C 
 
   if (temperatureC < tempThreshold) {
