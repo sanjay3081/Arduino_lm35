c7b4f97f77a92b377ea7f7215a368b4c9fded462
Sanjay Prasanth A S <84510522+sanjay3081@users.noreply.github.com>
2023-11-08 17:20:24 +0530

    Update sketch_nov06a.ino

--- sketch_nov06a.ino
+++ sketch_nov06a.ino
@@ -14,15 +14,18 @@ void setup() {
 }
 
 void loop() {
-  int iValue = analogRead(l_Pin);  // Read LM35 sensor value
-  float voltage = (iValue / 1024.0) * 5.0;  // adc value converted to analog value
-  float temperatureC = (voltage - 0.5) * 100.0;  // Convert to temperature in °C 
+  int iValue = analogRead(l_Pin);  /* Read LM35 sensor value. 
+  The input value which we get from sensor is connected to analog reader.
+  A0 pin will not directly give analog value. We get adc value from A0 pin*/
+  float voltage = (iValue / 1024.0) * 5.0;  // in arduino, we are using 10 bit adc and 5V as board voltage.
+  float temperatureC = (voltage - 0.5) * 100.0;  /* as the temp value for 0.5V is 0C so we are subtracting.
+  and we are multipying it with 1000 to convert V to mV and as 1C temperature changes for 10mv so we divide it. */
 
-  if (temperatureC < tempThreshold) {
+  if (temperatureC < tempThreshold) { //if input temperature is less than 30C this condition will run
     if (ledBlinkDelay1.update()) {
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // Toggle onboard LED
     }
-  } else {
+  } else { // it is always assigned that the input temperature is always greater than 30C
     if (ledBlinkDelay2.update()) {
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // Toggle onboard LED
     }
